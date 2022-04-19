6eaa1c3990dffcd351acd2849175c7628f3152a0
Vlad Ionescu <vionescu@fb.com>
2022-04-18 23:22:42 -0700

    Set 2022 dates and update theme

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Use the latest jekyll container to serve this directory for development purposes
+<<<<<<< HEAD
 docker run -p 4000:4000 --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll jekyll serve --livereload
+=======
+docker run -p 4000:4000 --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll jekyll serve --livereload
+>>>>>>> 26f8c05 (Set 2022 dates and update theme)
