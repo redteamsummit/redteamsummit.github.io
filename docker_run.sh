7da24b9dce1b51f03af395182705ac5942c0bc56
Vlad Ionescu <vionescu@fb.com>
2022-04-21 12:41:20 -0700

    Latest jekyll container is broken, use last good one for now

--- docker_run.sh
+++ docker_run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Use the latest jekyll container to serve this directory for development purposes
-<<<<<<< HEAD
-docker run -p 4000:4000 --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll jekyll serve --livereload
-=======
-docker run -p 4000:4000 --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll jekyll serve --livereload
->>>>>>> 26f8c05 (Set 2022 dates and update theme)
+# Use an older jekyll container to serve this directory for development purposes
+# Latest doesn't work as of April 2022 because they broke it. Webrick is no longer included.
+docker run -p 4000:4000 --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll:4.2.0 jekyll serve --livereload
