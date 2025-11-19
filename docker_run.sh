18d09dedbea1c59caf5aa9f212df7cb3ffd550b1
0xdade <0xdade@users.noreply.github.com>
2025-11-18 19:05:59 -0800

    Remove unused files

--- docker_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Use an older jekyll container to serve this directory for development purposes
-# Latest doesn't work as of April 2022 because they broke it. Webrick is no longer included.
-docker run -p 4000:4000 --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll:4.2.0 jekyll serve --livereload
